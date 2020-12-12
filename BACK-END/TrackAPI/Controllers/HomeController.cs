673212f39366100b9eff009fc2b940f84fded047
Mohammad Sakhidel <mohammad.sakhidel@gmail.com>
2020-12-12 21:13:28 +0330

    TrackAPI -> Home controller with actions for testing API added.

---
+++ BACK-END/TrackAPI/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackAPI.Controllers {
+
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok("Welcome to AussieTracker API.");
+        }
+
+        [HttpPost]
+        public IActionResult Post(dynamic data) {
+            return Ok(data ?? "");
+        }
+
+    }
+
+}
