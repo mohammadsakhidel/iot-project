73da40def1b46ff62ecd52fdfa6dbbe04d45fd85
unknown <mohammad.sakhidel@gmail.com>
2021-07-05 13:45:18 +0430

    SPA: main layout created.

--- FRONT-END/track-spa/src/layouts/MainLayout/index.tsx
+++ FRONT-END/track-spa/src/layouts/MainLayout/index.tsx
@@ -1,4 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Container, Toolbar, Typography, makeStyles, IconButton, Button } from '@material-ui/core';
+import { MenuOutlined } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import * as Routes from '../../constants/route-names';
+import * as Vars from '../../styles/vars';
+
+const useStyles = makeStyles((theme) => ({
+    appbarContainer: {
+        background: theme.palette.primary.main
+    },
+    title: {
+        flexGrow: 1
+    },
+    menuButton: {
+
+    },
+    content: {
+        marginTop: theme.spacing(2)
+    },
+    footer: {
+        marginTop: theme.spacing(2),
+        borderTop: `solid 1px ${Vars.COLOR_GRAY_L3}`,
+        padding: theme.spacing(1),
+        color: Vars.COLOR_GRAY_L1,
+        textAlign: "center"
+    }
+}));
 
 export default function MainLayout(props: any) {
 
@@ -6,9 +33,42 @@ export default function MainLayout(props: any) {
         children
     } = props;
 
+    // Hooks:
+    const history = useHistory();
+    const classes = useStyles();
+
+    // Event Handlers:
+    const onLoginClick = () => {
+        history.push(Routes.LOGIN);
+    };
+
     return (
-        <div style={{ border: 'solid 1px red' }}>
-            {children}
-        </div>
+        <>
+            <div className={classes.appbarContainer}>
+                <Container>
+                    <AppBar position="static" elevation={0}>
+                        <Toolbar>
+                            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                                <MenuOutlined />
+                            </IconButton>
+                            <Typography variant="h6" className={classes.title} color="secondary">
+                                ozTracker
+                            </Typography>
+                            <Button color="inherit" onClick={onLoginClick}>Login</Button>
+                        </Toolbar>
+                    </AppBar>
+                </Container>
+            </div>
+            <Container className={classes.content}>
+                <div>
+                    {children}
+                </div>
+                <div className={classes.footer}>
+                    <Typography variant="body2">
+                        Developed by: Mohammad Sakhidel
+                    </Typography>
+                </div>
+            </Container>
+        </>
     )
 }
--- FRONT-END/track-spa/src/pages/App/index.tsx
+++ FRONT-END/track-spa/src/pages/App/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GuardedRoute from '../../components/GuardedRoute';
 import * as Routes from '../../constants/route-names';
@@ -7,24 +8,49 @@ import MainLayout from '../../layouts/MainLayout';
 import Home from '../Home';
 import Login from '../Login';
 import Panel from '../Panel';
+import * as Vars from '../../styles/vars';
 
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: Vars.COLOR_PRIMARY,
+      light: Vars.COLOR_PRIMARY_L2,
+      dark: Vars.COLOR_PRIMARY_D1
+    },
+    primary: {
+      main: Vars.COLOR_SECONDARY,
+      light: Vars.COLOR_SECONDARY_L2,
+      dark: Vars.COLOR_SECONDARY_D1
+    }
+  },
+  mixins: {
+    toolbar: {
+      minHeight: 70
+    }
+  },
+  typography: {
+    fontFamily: "Quicksand, Roboto, Arial, sans-serif"
+  }
+});
 
 function App() {
   return (
     <Router>
-      <MainLayout>
-        <Switch>
-          <Route exact path={Routes.HOME}>
-            <Home />
-          </Route>
-          <Route path={Routes.LOGIN}>
-            <Login />
-          </Route>
-          <GuardedRoute path={Routes.PANEL} isAuthenticated={true}>
-            <Panel />
-          </GuardedRoute>
-        </Switch>
-      </MainLayout>
+      <ThemeProvider theme={theme}>
+        <MainLayout>
+          <Switch>
+            <Route exact path={Routes.HOME}>
+              <Home />
+            </Route>
+            <Route path={Routes.LOGIN}>
+              <Login />
+            </Route>
+            <GuardedRoute path={Routes.PANEL} isAuthenticated={true}>
+              <Panel />
+            </GuardedRoute>
+          </Switch>
+        </MainLayout>
+      </ThemeProvider>
     </Router>
   );
 }
